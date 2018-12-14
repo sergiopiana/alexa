8b837b19e87ba815c6a5ffe6e9388809a4b5b66c
sergiopiana <sergiopiana@gmail.com>
2018-12-14 13:46:16 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ app.listen(4000, () => {
  console.log("Server running on port 4000");
 });
 app.set('json spaces', 2)
-app.get("/api/alexa", (req, res, next) => {
+app.post("/api/alexa", (req, res, next) => {
     res.type('application/json'); 
     res.json(
 {
